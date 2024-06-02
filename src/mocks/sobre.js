422c90e6a8508d580d4adecfe7d3f33612c62417
1gorlopes <145722276+1gorlopes@users.noreply.github.com>
2024-06-02 12:10:32 -0300

    Add files via upload

---
+++ src/mocks/sobre.js
@@ -0,0 +1,13 @@
+import Logo from '../../assets/logoblack.png';
+import Img_Producao from '../../assets/juice2.png';
+
+const sobre = {
+    textos: {
+        logo: Logo,
+        img_producao: Img_Producao,
+        historia: "Nos corações das ruas pulsantes da cidade, onde o asfalto é o palco e a arte é a linguagem, surge um epicentro de expressão e estilo. Bem-vindo à Urban WRLD, uma fusão de moda e cultura que ecoa os batimentos de uma geração.Nas entranhas dessa loja, cada peça de roupa conta uma história, uma narrativa entrelaçada com o legado de um ícone perdido muito cedo. Juice Wrld, um poeta do rap moderno, imortalizado em nosso título como uma homenagem à sua visão e paixão pela criatividade urbana.A Urban WRLD não é apenas uma loja; é um santuário onde a energia das ruas se manifesta em cada tecido, em cada acessório meticulosamente selecionado. Desde camisetas graficamente ousadas até bonés que clamam por atenção, cada item é um grito de liberdade, uma afirmação de identidade. Então, junte-se a nós nesta jornada. Explore, descubra e celebre a revolução urbana na Urban WRLD - onde a moda encontra a cultura e o espírito de Juice Wrld vive eternamente.",
+        texto_imagem: "- One thing my heart tells me is, flex on a h0e every time they're insecure."
+    }
+}
+
+export default sobre;
